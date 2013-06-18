e8ab0ef1ee9671d38d04ddd5941f94910ea912b4
keith kirk <kkirk@undergroundelephant.com>
2013-06-18 21:41:12 +0000

    corrected namespacing

--- src/app.php
+++ src/app.php
@@ -1,18 +1,18 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use UE\Application\PresentationApplication;
+use MarkdownPresentation\Application\PresentationApplication;
 
 $app = new PresentationApplication();
 
 $app->register( new Silex\Provider\TwigServiceProvider(), [ 'twig.path' => __DIR__.'/../web/views' ] );
 $app->register( new Nicl\Silex\MarkdownServiceProvider(), [ 'markdown.parser' => 'extra' ] );
-$app->register( new UE\Provider\SnappyServiceProvider() );
+$app->register( new MarkdownPresentation\Provider\SnappyServiceProvider() );
 $app->register( new Silex\Provider\MonologServiceProvider(), [ 
 	'monolog.logfile' => '/var/log/apache2/error.log', 
 	'monolog.level' => \Monolog\Logger::WARNING ] 
 );
-$app->register( new UE\Provider\MongoClientProvider(), [ 
+$app->register( new MarkdownPresentation\Provider\MongoClientProvider(), [ 
 	'mongo.host' => "mongodb://example.com:27017",
 	'mongo.db'	=> "mdpresents",
 	'mongo.collection' => "slides",
