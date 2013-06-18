efcae8a7c8719455ae6fdf43e6da5129aff20203
keith kirk <kkirk@undergroundelephant.com>
2013-06-18 23:16:07 +0000

    switched presentation view to use Mousetrap keybinds and increased font size scaling

--- web/scripts/presentation.js
+++ web/scripts/presentation.js
@@ -16,28 +16,26 @@ $(document).ready( function() {
 			return false;
 		}
 	});
-	$(document).keydown( function( e ) {
-		if ( e.which == 39 || e.which == 32 || e.which == 34 || e.which == 40) {
-			next = $('div.page:visible').next('div.page');
-			if ( next.length ) {
-				$('div.page:visible').hide();
-				next.fadeIn( 200 );
-				return false;
-			}
+	Mousetrap.bind([ 'right', 'space', 'pagedown', 'down' ], function( e ) {
+		next = $('div.page:visible').next('div.page');
+		if ( next.length ) {
+			$('div.page:visible').hide();
+			next.fadeIn( 200 );
+			return false;
 		}
-		if ( e.which == 37 || e.which == 8 || e.which == 33 || e.which == 38 ) {
-			last = $('div.page:visible').prev('div.page');
-			if ( last.length ) {
-				$('div.page:visible').hide();
-				last.fadeIn( 200 );
-				return false;
-			}
+	});
+	Mousetrap.bind([ 'left', 'backspace', 'pageup', 'up' ], function( e ) {
+		last = $('div.page:visible').prev('div.page');
+		if ( last.length ) {
+			$('div.page:visible').hide();
+			last.fadeIn( 200 );
+			return false;
 		}
 	});
 });
 var fontFix = function () {
 	var width = window.innerWidth || document.documentElement.clientWidth;
-	document.getElementById("document").style.fontSize = Math.max( 1, ( ( 24 / 1920 ) * width ) / 12 ) + "em";
+	document.getElementById("document").style.fontSize = Math.max( 1, ( ( 24 / 1920 ) * width ) / 9 ) + "em";
 };
 window.addEventListener('resize', fontFix);
 window.addEventListener('load', fontFix);
